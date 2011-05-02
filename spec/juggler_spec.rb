539ac99a3173bc90708950a925c90ed500caa825
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2011-05-02 11:16:23 -0700

    Refactored setup harnesses to be DRYer.

--- spec/juggler_spec.rb
+++ spec/juggler_spec.rb
@@ -1,68 +1,62 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-def mock_s3_object(overrides={})
-  mock(AWS::S3::S3Object, {
-    :path => '/path/to/file', :rename => nil, :delete => nil, :value => 'val'
-  }.merge(overrides))
-end
+describe Juggler do
 
-describe Juggler::Worker do
+  def mock_s3_object(overrides={})
+    mock(AWS::S3::S3Object, {
+      :path => '/path/to/file', :rename => nil, :delete => nil, :value => 'val'
+    }.merge(overrides))
+  end
 
   before :each do 
     @bucket = mock(AWS::S3::Bucket, :objects => [ ], :[] => nil)
     AWS::S3::Bucket.stub!(:find).
       and_return(@bucket)
     AWS::S3::S3Object.stub!(:store)
-  end
 
-  it "should poll for whether there is a file in the queue" do
-    Juggler.queue_bucket.should_receive(:objects)
-    Juggler::Worker.run
-  end
-
-  context "when there is a file in the queue" do
-    before :each do
-      @s3_object = mock_s3_object
-      Juggler.queue_bucket.stub!(:objects).and_return([ @s3_object ])
-      Juggler.queue_bucket.stub!(:[]).and_return(@s3_object)
-    end
-
-    it "should process the file" do
-      Juggler::Job.should_receive(:run).with(@s3_object)
-      Juggler::Worker.run
-    end
-  end
-
-end
-
-describe Juggler::Job do
-
-  before :each do
-    @bucket = mock(AWS::S3::Bucket, :objects => [ ], :[] => nil)
-    @s3_object = mock_s3_object(:path => "/the_bucket/filename")
-    AWS::S3::Bucket.stub!(:find).
-      and_return(@bucket)
-    AWS::S3::S3Object.stub!(:store)
+    @s3_object = mock_s3_object
+    Juggler.queue_bucket.stub!(:objects).and_return([ @s3_object ])
     Juggler.queue_bucket.stub!(:[]).and_return(@s3_object)
   end
 
-  context "and the file already has the lock prefix" do
-    before :each do
-      @s3_object.stub!(:path).and_return("---LOCKED---foobar")
+  describe Juggler::Worker do
+
+    it "should poll for whether there is a file in the queue" do
+      Juggler.queue_bucket.should_receive(:objects)
+      Juggler::Worker.run
     end
 
-    it "should skip processing" do
+    context "when there is a file in the queue" do
+      it "should process the file" do
+        Juggler::Job.should_receive(:run).with(@s3_object)
+        Juggler::Worker.run
+      end
+    end
+
+  end
+
+  describe Juggler::Job do
+
+    before :each do
+      @s3_object = mock_s3_object(:path => "/the_bucket/filename")
+      Juggler.queue_bucket.stub!(:[]).and_return(@s3_object)
+    end
+
+    context "and the file already has the lock prefix" do
+      before :each do
+        @s3_object.stub!(:path).and_return("---LOCKED---foobar")
+      end
+
+      it "should skip processing" do
+        Juggler::Job.run(@s3_object)
+      end
+    end
+
+    it "should prepend a lock prefix to the object path" do
+      @s3_object.should_receive(:rename).with("---LOCKED---filename")
       Juggler::Job.run(@s3_object)
     end
-  end
 
-  it "should prepend a lock prefix to the object path" do
-    @s3_object.should_receive(:rename).with("---LOCKED---filename")
-    Juggler::Job.run(@s3_object)
-  end
-
-  it "should process the file through the processor" do
-
+    it "should process the file through the processor"
   end
 end
-
