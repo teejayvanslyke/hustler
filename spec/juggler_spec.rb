284ea85e52948e5eacd680f2323dc06b62ab75da
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2011-05-02 11:13:58 -0700

    All specs pass. Now to whittle down the repetition in them.

--- spec/juggler_spec.rb
+++ spec/juggler_spec.rb
@@ -1,11 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+def mock_s3_object(overrides={})
+  mock(AWS::S3::S3Object, {
+    :path => '/path/to/file', :rename => nil, :delete => nil, :value => 'val'
+  }.merge(overrides))
+end
+
 describe Juggler::Worker do
 
   before :each do 
     @bucket = mock(AWS::S3::Bucket, :objects => [ ], :[] => nil)
     AWS::S3::Bucket.stub!(:find).
       and_return(@bucket)
+    AWS::S3::S3Object.stub!(:store)
   end
 
   it "should poll for whether there is a file in the queue" do
@@ -15,12 +22,13 @@ describe Juggler::Worker do
 
   context "when there is a file in the queue" do
     before :each do
-      @s3_object = mock(AWS::S3::S3Object, :path => '/path/to/file', :rename => nil)
+      @s3_object = mock_s3_object
       Juggler.queue_bucket.stub!(:objects).and_return([ @s3_object ])
+      Juggler.queue_bucket.stub!(:[]).and_return(@s3_object)
     end
 
     it "should process the file" do
-      Juggler::Job.should_receive(:new).with(@s3_object)
+      Juggler::Job.should_receive(:run).with(@s3_object)
       Juggler::Worker.run
     end
   end
@@ -30,7 +38,12 @@ end
 describe Juggler::Job do
 
   before :each do
-    @s3_object = mock(AWS::S3::S3Object, :path => "/the_bucket/filename")
+    @bucket = mock(AWS::S3::Bucket, :objects => [ ], :[] => nil)
+    @s3_object = mock_s3_object(:path => "/the_bucket/filename")
+    AWS::S3::Bucket.stub!(:find).
+      and_return(@bucket)
+    AWS::S3::S3Object.stub!(:store)
+    Juggler.queue_bucket.stub!(:[]).and_return(@s3_object)
   end
 
   context "and the file already has the lock prefix" do
